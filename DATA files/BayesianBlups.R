34af475a306baea334c549f7265be088ee49bc92
SSalaria5 <117963789+SSalaria5@users.noreply.github.com>
2023-02-09 18:10:57 -0500

    Add files via upload

--- DATA files/BayesianBlups.R
+++ DATA files/BayesianBlups.R
@@ -8,7 +8,7 @@ df <- read.csv("DTST.data.csv", row.names = 1)
 df[,1:6] <- lapply(df[,1:6], function(x) as.factor(x))
 head(df)
 
-# y ~ GEN + BLOCK(ENV:REP) + REP(ENV) + ENV + GEN:ENV
+
 bayesian_blups <- df[,c(5,6)]
 for (trait in c("PalmiticAcid","LinoleicAcid","AlphaLinolenicAcid","OleicAcid")){
   stan.model <- stan_lmer(paste0(trait, 
@@ -21,6 +21,5 @@ for (trait in c("PalmiticAcid","LinoleicAcid","AlphaLinolenicAcid","OleicAcid"))
 }
 write.csv(unique(bayesian_blups), "bayesian_blups.csv", row.names = F, quote=F)
 
-launch_shinystan(stan.model)
-hist(ranef(stan.model)$GenotypeNum[,1])
+
 
